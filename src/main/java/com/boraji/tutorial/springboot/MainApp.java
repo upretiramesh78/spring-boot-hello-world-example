181526e1b23f9648cd3560635dc7c5a0c29ce836
Ramesh Chandra <upreti.ramesh@gmail.com>
2019-06-02 13:45:04 +0200

    commit new change

--- src/main/java/com/boraji/tutorial/springboot/MainApp.java
+++ src/main/java/com/boraji/tutorial/springboot/MainApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
    public static void main(String[] args) {
       SpringApplication.run(MainApp.class, args);
+      System.out.println("hello");
    }
 }
